568b36bce0a934ac45331a9b6b1e7f71821e561b
Ivan Gomes <ivanjunior.pt@gmail.com>
2022-05-16 14:27:47 -0300

    listening to more events

--- src/pages/api/_lib/menageSubscription.ts
+++ src/pages/api/_lib/menageSubscription.ts
@@ -1,9 +1,60 @@
+import { fauna } from "../../../services/fauna"
+import { query as q } from "faunadb"
+import { stripe } from "../../../services/stripe"
+
 //Salvar inscrição no banco de dados
 export async function saveSubscription(
     subscriptionId: string,
     customerId: string,
+    createAction = false,
 ) {
-   // Buscar o usuário no FaunaDB com o ID {stripe.customer.id}
-   // Salvar os dados da subscription no FaunaDB
-   console.log(subscriptionId, customerId)
+    // Buscar o usuário no FaunaDB com o ID {stripe.customer.id}
+    // Salvar os dados da subscription no FaunaDB
+    // console.log(subscriptionId, customerId)
+    // Ref do usuário no FaunaDB = relacionamentos do banco
+    const userRef = await fauna.query(
+        q.Select(
+            "ref",
+            q.Get(
+                q.Match(
+                    q.Index('user_by_stripe_customer_id'),
+                    customerId
+                )
+            )
+        )
+    )
+
+    const subscription = await stripe.subscriptions.retrieve(subscriptionId)
+
+    const subscriptionData = {
+        id: subscription.id,
+        userId: userRef,
+        status: subscription.status,
+        price_id: subscription.items.data[0].price.id,
+
+    }
+
+    if (createAction) {
+        await fauna.query(
+            q.Create(
+                q.Collection('subscriptions'),
+                { data: subscriptionData }
+            )
+        )
+    } else {
+        await fauna.query(
+            q.Replace(
+                q.Select(
+                    "ref",
+                    q.Get(
+                        q.Match(
+                            q.Index('subscription_by_id'),
+                            subscriptionId,
+                        )
+                    )
+                ),
+                { data: subscriptionData }
+            )
+        )
+    }
 } 
--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -16,6 +16,8 @@ async function buffer(readable: Readable) {
             typeof chunk === "string" ? Buffer.from(chunk) : chunk
         )
     }
+
+    return Buffer.concat(chunks)
 }
 
 export const config = { // Desabilitando o entendimento padrão do Next, do que está vindo da requisição. Isso ocorre porque 'stream' é uma string
@@ -25,12 +27,14 @@ export const config = { // Desabilitando o entendimento padrão do Next, do que 
 }
 
 const relevantEvents = new Set([
-    'checkout.session.completed'
+    'checkout.session.completed',
+    'customer.subscription.updated',
+    'customer.subscription.deleted',
 ])
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'POST') {
-        const buf: any = await buffer(req)
+        const buf = await buffer(req)
         const secret = req.headers['stripe-signature']
 
         let event: Stripe.Event
@@ -42,24 +46,40 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         }
 
         const { type } = event
+        console.log({ type })
 
         if (relevantEvents.has(type)) {
             try {
                 switch (type) {
+                    case 'customer.subscription.updated':
+                    case 'customer.subscription.deleted':
+
+                        const subscription = event.data.object as Stripe.Subscription;
+
+                        await saveSubscription(
+                            subscription.id,
+                            subscription.customer.toString(),
+                            false
+                        )
+
+                        break;
+
                     case 'checkout.session.completed':
 
                         const checkoutSession = event.data.object as Stripe.Checkout.Session
 
                         await saveSubscription(
                             checkoutSession.subscription.toString(),
-                            checkoutSession.customer.toString()
-                        )    
+                            checkoutSession.customer.toString(),
+                            true
+                        )
 
                         break;
                     default:
                         throw new Error('Unhand event.')
                 }
             } catch (err) {
+                console.log(err)
                 return res.json({ error: 'Webhook handle failed.' })
             }
         }

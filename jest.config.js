f97a30a2207195d22c33f71d4dd1a5982951ae00
Ivan Gomes <ivanjunior.pt@gmail.com>
2022-07-01 10:19:38 -0300

    configuring tests

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testPathIgnorePatterns: ["/node_modules", "/.next"], // quais pastas os testes vão ignorar
+  setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+  },
+  testEnviroment: "jsdom",
+};

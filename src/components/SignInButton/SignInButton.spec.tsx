fd8b966da16dc7e890af1b11216e76375b6b148a
Ivan Gomes <ivanjunior.pt@gmail.com>
2022-07-04 10:56:46 -0300

    fix jest mock

--- src/components/SignInButton/SignInButton.spec.tsx
+++ src/components/SignInButton/SignInButton.spec.tsx
@@ -1,20 +1,13 @@
 import { render, screen } from "@testing-library/react";
-import { mocked } from "jest-mock";
 import { useSession } from "next-auth/react";
 import { SignInButton } from ".";
 
-jest.mock("next-auth/react", () => {
-  return {
-    useSession() {
-      return [null, false];
-    },
-  };
-});
+jest.mock("next-auth/react");
 
 describe("SignInButton component", () => {
   // a descrição do que está sendo testado
   it("renders correctly when user is not authenticated", () => {
-    const useSessionMocked = jest.fn(useSession);
+    const useSessionMocked = jest.mocked(useSession);
 
     useSessionMocked.mockReturnValueOnce({ data: null, status: "loading" });
 
@@ -27,12 +20,14 @@ describe("SignInButton component", () => {
   });
 
   it("renders correctly when user is authenticated", () => {
-    const useSessionMocked = jest.fn(useSession);
+    const useSessionMocked = jest.mocked(useSession);
 
     useSessionMocked.mockReturnValueOnce({
       data: {
-        name: "Jhon Doe",
-        email: "jhon.doe@example.com",
+        user: {
+          name: "Jhon Doe",
+          email: "jhon.doe@example.com",
+        },
         expires: "fake-expires",
       },
       status: "authenticated",
